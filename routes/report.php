d7442fb49dfa877b18857259325e41d4c281386f
Apurba Singh <aprubansinghdev@gmail.com>
2025-11-20 01:34:46 +0600

    Fixed route issue

--- routes/report.php
+++ routes/report.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Report\ServiceReportsController;
+
 Route::group(['prefix' => 'report', 'middleware' => ['auth']], function () {
 
     //Service Module Start
-Route::any('prescription-history/{param?}', 'Report\ServiceReportsController@prescriptionHistory')->name('prescriptionreports.prescriptionHistory');
+    Route::any('prescription-history/{param?}', [ServiceReportsController::class, 'prescriptionHistory'])->name('prescriptionreports.prescriptionHistory');
 
 });
